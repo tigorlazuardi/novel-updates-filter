7a3f4cb4b10e1a97f35d61cee2fd925f8221473b
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2020-08-10 13:50:25 +0700

    fixed inject into tag

--- src/userscript.ts
+++ src/userscript.ts
@@ -11,7 +11,7 @@
 // @version      1.0.0
 // @description  Filters the result by country
 // @run-at       document-end
-// @inject-in    to content
+// @inject-into  content
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @license      MIT
