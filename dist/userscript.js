c2c99f3c7e42c0dd37ef7fee1ca9a449329dea9c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2020-08-17 21:56:42 +0700

    changed button style and now added home support

--- dist/userscript.js
+++ dist/userscript.js
@@ -8,6 +8,7 @@
 // @include      https://www.novelupdates.com/series-finder/*
 // @include      https://www.novelupdates.com/latest-series/*
 // @include      https://www.novelupdates.com/viewlist/*
+// @match        https://www.novelupdates.com/
 // @version      1.0.1
 // @description  Filters the result by country
 // @run-at       document-end
@@ -21,7 +22,7 @@
 // ==/UserScript==
 // This script is written in typescript and thus ugly to read if read directly like this.
 // Please refer to git page (the name space above) for the actual source code
-var _a;
+var _a, _b, _c, _d;
 var origins = {
     Korea: 'orgkr',
     China: 'orgcn',
@@ -32,62 +33,84 @@ var origins = {
     Thailand: 'orgth',
     Vietnam: 'orgvn',
 };
-function setStatus(selector, status) {
-    var el = document.querySelectorAll("." + selector);
+var home = document.querySelectorAll('.l-content table#myTable')[1];
+var injectLoc = (_b = (_a = 
+// home
+home !== null && 
+// home
+home !== void 0 ? 
+// home
+home : 
+// Series Listing and Series Ranking
+document.querySelector('.search_sort')) !== null && _a !== void 0 ? _a : 
+// Series Finder and Latest Series
+document.querySelector('#rankfilter')) !== null && _b !== void 0 ? _b : 
+// Rec Lists
+document.querySelector('.ucl_main');
+function setDisplay(selector, display) {
+    var el = document.querySelectorAll(selector);
     el.forEach(function (e) {
-        var _a, _b, _c;
-        // @ts-ignore
-        (_c = (_b = (_a = e.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.parentElement) === null || _c === void 0 ? void 0 : _c.style.display = status;
+        return home
+            ? display === 'block'
+                ? // compability with site. Seems display block destroys the view.
+                    e.parentElement.parentElement.removeAttribute('style')
+                : (e.parentElement.parentElement.style.display = display)
+            : (e.parentElement.parentElement.parentElement.style.display = display);
     });
 }
 function toggleFilter() {
-    var ok = this.checked;
-    if (ok) {
-        setStatus(this.value, 'none');
+    var prev = GM_getValue(this.id, false);
+    var hide = !prev;
+    if (hide) {
+        home
+            ? setDisplay(".l-content > table#myTable ." + origins[this.id], 'none')
+            : setDisplay("." + origins[this.id], 'none');
+        this.style.backgroundColor = '#F9F871';
     }
     else {
-        setStatus(this.value, 'block');
+        home
+            ? setDisplay(".l-content > table#myTable ." + origins[this.id], 'block')
+            : setDisplay("." + origins[this.id], 'block');
+        this.style.backgroundColor = 'white';
     }
-    GM_setValue(this.name, ok);
+    GM_setValue(this.id, hide);
 }
-var injectLoc = 
-// Series Listing and Series Ranking
-document.querySelector('.search_sort') ||
-    // Series Finder and Latest Series
-    document.querySelector('#rankfilter') ||
-    // Rec Lists
-    document.querySelector('.ucl_main');
 var filterArea = document.createElement('div');
 var filterLabel = document.createElement('p');
+// Styling Blocklist text
 filterLabel.innerHTML = '<b>Blocklist :</b>';
 filterLabel.style.marginBottom = '0';
 var selections = document.createElement('div');
+// Making sure the chexboxes wrap well on small resolution screens
 selections.style.display = 'flex';
 selections.style.flexWrap = 'wrap';
 for (var k in origins) {
     var status_1 = GM_getValue(k, false);
-    var container = document.createElement('div');
-    var checkbox = document.createElement('input');
-    var label = document.createElement('label');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.name = k;
-    checkbox.value = origins[k];
-    checkbox.addEventListener('click', toggleFilter);
-    checkbox.checked = status_1;
+    var item = document.createElement('div');
+    var name_1 = document.createElement('p');
+    name_1.style.marginBottom = '0';
+    name_1.innerText = k;
+    // Making sure checkbox is inline with label
+    item.appendChild(name_1);
+    item.style.display = 'flex';
+    item.style.alignItems = 'center';
+    item.style.justifyContent = 'center';
+    item.style.minWidth = '100px';
+    item.style.backgroundColor = 'white';
+    item.style.padding = '5px';
+    item.style.marginBottom = '15px';
+    item.style.marginTop = '5px';
+    item.style.marginRight = '5px';
+    item.style.cursor = 'pointer';
+    item.style.borderRadius = '10px';
+    item.style.border = '2px solid #7c5262';
+    item.id = k;
+    item.addEventListener('click', toggleFilter);
     if (status_1) {
-        setStatus(origins[k], 'none');
+        setDisplay("." + origins[k], 'none');
+        item.style.backgroundColor = '#f9f871';
     }
-    label.setAttribute('for', k);
-    label.innerText = k;
-    container.appendChild(checkbox);
-    container.appendChild(label);
-    container.style.display = 'flex';
-    container.style.flexWrap = 'wrap';
-    container.style.marginRight = '10px';
-    container.style.alignItems = 'center';
-    container.style.justifyContent = 'center';
-    container.style.minWidth = '50px';
-    selections.appendChild(container);
+    selections.appendChild(item);
 }
 filterArea.appendChild(filterLabel);
 filterArea.appendChild(selections);
@@ -99,4 +122,5 @@ filterArea.style.borderRadius = '10px';
 filterArea.style.border = '2px solid #B8CB99';
 filterArea.style.paddingLeft = '10px';
 filterArea.style.paddingRight = '10px';
-(_a = injectLoc === null || injectLoc === void 0 ? void 0 : injectLoc.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(filterArea, injectLoc.nextSibling);
+home
+    ? (_c = injectLoc === null || injectLoc === void 0 ? void 0 : injectLoc.parentNode) === null || _c === void 0 ? void 0 : _c.insertBefore(filterArea, injectLoc) : (_d = injectLoc === null || injectLoc === void 0 ? void 0 : injectLoc.parentNode) === null || _d === void 0 ? void 0 : _d.insertBefore(filterArea, injectLoc.nextSibling);
--- src/userscript.ts
+++ src/userscript.ts
@@ -50,22 +50,32 @@ const injectLoc =
   document.querySelector('.ucl_main')
 
 function setDisplay(selector: string, display: 'none' | 'block') {
-  const el = document.querySelectorAll(`.${selector}`)
+  const el = document.querySelectorAll(selector)
   el.forEach((e) =>
     home
-      ? (e.parentElement.parentElement.style.display = display)
+      ? display === 'block'
+        ? // compability with site. Seems display block destroys the view.
+          e.parentElement.parentElement.removeAttribute('style')
+        : (e.parentElement.parentElement.style.display = display)
       : (e.parentElement.parentElement.parentElement.style.display = display),
   )
 }
 
-function toggleFilter(this: HTMLInputElement) {
-  const hide = this.checked
+function toggleFilter(this: HTMLDivElement) {
+  const prev = GM_getValue<boolean>(this.id, false)
+  const hide = !prev
   if (hide) {
-    setDisplay(this.value, 'none')
+    home
+      ? setDisplay(`.l-content > table#myTable .${origins[this.id]}`, 'none')
+      : setDisplay(`.${origins[this.id]}`, 'none')
+    this.style.backgroundColor = '#F9F871'
   } else {
-    setDisplay(this.value, 'block')
+    home
+      ? setDisplay(`.l-content > table#myTable .${origins[this.id]}`, 'block')
+      : setDisplay(`.${origins[this.id]}`, 'block')
+    this.style.backgroundColor = 'white'
   }
-  GM_setValue(this.name, hide)
+  GM_setValue(this.id, hide)
 }
 
 const filterArea = document.createElement('div')
@@ -82,34 +92,35 @@ selections.style.flexWrap = 'wrap'
 
 for (const k in origins) {
   const status = GM_getValue<boolean>(k, false)
-  const items = document.createElement('div')
-  const checkbox = document.createElement('input')
-  const label = document.createElement('label')
-  checkbox.setAttribute('type', 'checkbox')
-  checkbox.name = k
-  checkbox.value = origins[k]
-  checkbox.addEventListener('click', toggleFilter)
-  checkbox.checked = status
-  checkbox.style.position = 'static'
+  const item = document.createElement('div')
+  const name = document.createElement('p')
+  name.style.marginBottom = '0'
 
-  if (status) {
-    setDisplay(origins[k], 'none')
-  }
-
-  label.setAttribute('for', k)
-  label.innerText = k
+  name.innerText = k
 
   // Making sure checkbox is inline with label
-  items.appendChild(checkbox)
-  items.appendChild(label)
-  items.style.display = 'flex'
-  items.style.flexWrap = 'wrap'
-  items.style.marginRight = '10px'
-  items.style.alignItems = 'center'
-  items.style.justifyContent = 'center'
-  items.style.minWidth = '50px'
+  item.appendChild(name)
+  item.style.display = 'flex'
+  item.style.alignItems = 'center'
+  item.style.justifyContent = 'center'
+  item.style.minWidth = '100px'
+  item.style.backgroundColor = 'white'
+  item.style.padding = '5px'
+  item.style.marginBottom = '15px'
+  item.style.marginTop = '5px'
+  item.style.marginRight = '5px'
+  item.style.cursor = 'pointer'
+  item.style.borderRadius = '10px'
+  item.style.border = '2px solid #7c5262'
+  item.id = k
+  item.addEventListener('click', toggleFilter)
 
-  selections.appendChild(items)
+  if (status) {
+    setDisplay(`.${origins[k]}`, 'none')
+    item.style.backgroundColor = '#f9f871'
+  }
+
+  selections.appendChild(item)
 }
 
 filterArea.appendChild(filterLabel)
